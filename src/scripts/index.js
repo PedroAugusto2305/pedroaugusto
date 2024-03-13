ea55f3f6c53acd3f3c786c8f1807d1c1899d1a56
PedroAugusto2305 <p.augusto_23@outlook.com>
2024-03-12 21:45:24 -0300

    Init: create README and project scaffold

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+// ScrollReveal().reveal(".c-about__title", {
+//   distance: "150%",
+//   origin: "top",
+//   opacity: .1,
+//   delay: 200
+// });
+// ScrollReveal().reveal(".c-about__image", { 
+//     distance: "300%",
+//     origin: "left",
+//     opacity: .1,
+//     delay: 300,
+//     duration: 3000
+//  });
+
+//  ScrollReveal().reveal(".c-about__text", { 
+//     distance: "300%",
+//     origin: "right",
+//     opacity: .1,
+//     delay: 300,
+//     duration: 3000
+//  });
