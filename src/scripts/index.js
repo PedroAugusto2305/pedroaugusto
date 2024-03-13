6e8d07aace1fefcc62735d07ec92f1a34fd202f8
PedroAugusto2305 <p.augusto_23@outlook.com>
2024-03-13 09:58:11 -0300

    Fix: Navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,20 @@
-// ScrollReveal().reveal(".c-about__title", {
-//   distance: "150%",
-//   origin: "top",
-//   opacity: .1,
-//   delay: 200
-// });
-// ScrollReveal().reveal(".c-about__image", { 
-//     distance: "300%",
-//     origin: "left",
-//     opacity: .1,
-//     delay: 300,
-//     duration: 3000
-//  });
-
-//  ScrollReveal().reveal(".c-about__text", { 
-//     distance: "300%",
-//     origin: "right",
-//     opacity: .1,
-//     delay: 300,
-//     duration: 3000
-//  });
+document.addEventListener('DOMContentLoaded', () => {
+    // Identifica a URL atual
+    const urlAtual = window.location.href;
+  
+    // Seleciona os itens do menu
+    const sobreMimItem = document.querySelector('#about a');
+    const projetosItem = document.querySelector('#projects a');
+  
+    // Por padrão, assume que estamos na página "Sobre Mim"
+    sobreMimItem.classList.add('active');
+  
+    // Verifica se a URL atual corresponde à página "Projetos"
+    if (urlAtual.includes('projects.html')) {
+      // Remove a classe 'active' do item "Sobre Mim"
+      sobreMimItem.classList.remove('active');
+      // Adiciona a classe 'active' ao item "Projetos"
+      projetosItem.classList.add('active');
+    }
+  });
+  
